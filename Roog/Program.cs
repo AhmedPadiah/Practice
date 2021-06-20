30b6827db462f919d65ebf73df3690a057198a19
Ahmed Kobue <g19k0734@campus.ru.ac.za>
2021-06-21 01:02:18 +0200

    This is a modified file

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,7 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello , my name is Ahmed Kobue and am making changes to git");
         }
     }
 }
